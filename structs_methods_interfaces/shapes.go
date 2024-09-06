e52751145d4dba70126fc72de3390eefe516790e
Harry Parsons <harry@madepurple.com>
2024-09-06 09:51:01 +0100

    Structs - methods - interfaces completed

--- structs_methods_interfaces/shapes.go
+++ structs_methods_interfaces/shapes.go
@@ -2,6 +2,9 @@ package structs_methods_interfaces
 
 import "math"
 
+type Shape interface {
+	Area() float64
+}
 type Rectangle struct {
 	Width  float64
 	Height float64
@@ -19,6 +22,14 @@ func (c Circle) Area() float64 {
 	return math.Pi * (math.Pow(c.Radius, 2))
 }
 
+type Triangle struct {
+	Base   float64
+	Height float64
+}
+
+func (t Triangle) Area() float64 {
+	return (t.Base * t.Height) * 0.5
+}
 func Perimeter(r Rectangle) float64 {
 	return 2 * (r.Width + r.Height)
 }
